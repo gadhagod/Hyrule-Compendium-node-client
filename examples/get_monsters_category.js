8fc4c651687ba332c73d7e1ee0800053dcf8d2db
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-03-30 23:19:19 -0700

    Simplify examples

--- examples/get_monsters_category.js
+++ examples/get_monsters_category.js
@@ -2,6 +2,4 @@ const hyrule_compendium = require("hyrule-compendium")
 
 let comp = new hyrule_compendium.compendium
 
-comp.get_category("monsters", function(data){
-    console.log(data)
-})
+comp.get_category("monsters", console.log)
