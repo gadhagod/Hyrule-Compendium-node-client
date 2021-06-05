c52fa634b4b6787be1330c0440c6daada262637c
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-06-05 11:50:54 -0700

    Code formatting (MAJOR API CHANGE)

--- src/exceptions.ts
+++ src/exceptions.ts
@@ -1,35 +1,33 @@
-class NoCategoryError extends Error {
+export class NoCategoryError extends Error {
     /*
     Raised when a given category does not exist in the compendium
 
     Parameters:
-        * `target_category`: Non-existant input category that causes error.
+        * `targetCategory`: Non-existant input category that causes error.
             - type: string
     */
 
-    constructor(target_category: string) {
-        super("Category '" + String(target_category) + "' not found. Available categories are 'creatures', 'equipment', 'materials', 'monsters', or 'treasure'")
+    constructor(targetCategory: string) {
+        super("Category '" + String(targetCategory) + "' not found. Available categories are 'creatures', 'equipment', 'materials', 'monsters', or 'treasure'")
     }
 }
 
-class NoEntryError extends Error {
+export class NoEntryError extends Error {
     /*
     Raised when a given entry does not exist in the compendium
 
     Parameters:
-        * `target_entry`: Non-existant input entry that causes error.
+        * `targetEntry`: Non-existant input entry that causes error.
             - type: string
     */
 
-    constructor(target_entry: string|number){
-        if (typeof(target_entry) == "number"){
-            super("Entry with ID "+ String(target_entry) + " not found.")
-        } else if (typeof(target_entry) == "string"){
-            super("Entry with name '" + target_entry + "' not found.")
+    constructor(targetEntry: string|number){
+        if (typeof(targetEntry) == "number"){
+            super("Entry with ID "+ String(targetEntry) + " not found.")
+        } else if (typeof(targetEntry) == "string"){
+            super("Entry with name '" + targetEntry + "' not found.")
         } else {
-            super("Type '" + typeof(target_entry) + "' not a valid entry type. Must be INT or STR")
+            super("Type '" + typeof(targetEntry) + "' not a valid entry type. Must be INT or STR")
         }
     }
 }
-
-export { NoEntryError, NoCategoryError};
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const https = require("https");
 const Stream = require("stream").Transform
 const fs = require("fs");
 
-class compendium {
+export class HyruleCompendium {
     /*
     Base class for hyrule-compendium.
 
@@ -12,17 +12,17 @@ class compendium {
         * `url`: The base URL for the API.
             - default: "https://botw-compendium.herokuapp.com/api/v2"
             - type: string
-        * `default_timeout`: Default milliseconds to wait for response for all API calling functions until executing `error_callback`.
+        * `defaultTimeout`: Default milliseconds to wait for response for all API calling functions until executing `errorCallback`.
     */
 
     url: string
-    default_timeout: number
-    constructor(default_timeout=10000, url="https://botw-compendium.herokuapp.com/api/v2"){
-        this.default_timeout=default_timeout
+    defaultTimeout: number
+    constructor(defaultTimeout=10000, url="https://botw-compendium.herokuapp.com/api/v2"){
+        this.defaultTimeout=defaultTimeout
         this.url=url;
     }
 
-    get_entry(entry: string|number, callback: Function, timeout: number=this.default_timeout, error_callback: Function=function(err: any){throw err}){
+    getEntry(entry: string|number, callback: Function, timeout: number=this.defaultTimeout, errorCallback: Function=function(err: any){throw err}){
         /*
         Gets an entry from the compendium.
         Parameters:
@@ -30,10 +30,10 @@ class compendium {
                 - type: string, int
             * `callback`: Function to be executed with metadata on the entry.
                 - type: function
-            * `timeout`: Milliseconds to wait for response until executing `error_callback`.
+            * `timeout`: Milliseconds to wait for response until executing `errorCallback`.
                 - type: number
-                - default: `this.default_timeout`
-            * `error_callback`: Function to be executed on request errors.
+                - default: `this.defaultTimeout`
+            * `errorCallback`: Function to be executed on request errors.
                 - type: function
                 - default: Throws error
         */
@@ -51,11 +51,11 @@ class compendium {
                 }
                 callback(data);
             })
-        }).on("error", error_callback)
+        }).on("error", errorCallback)
         req.setTimeout(timeout, req.destroy);
     }
 
-    get_category(category: string, callback: Function, timeout: number=this.default_timeout, error_callback: Function=function(err: any){throw err}){
+    getCategory(category: string, callback: Function, timeout: number=this.defaultTimeout, errorCallback: Function=function(err: any){throw err}){
         /*
         Gets all entries from a category in the compendium.
 
@@ -65,10 +65,10 @@ class compendium {
                 - notes: must be in ["creatures", "equipment", "materials", "monsters", "treasure"]
             * `callback`: Function to be executed with all entries in the category.
                 - type: function
-            * `timeout`: Milliseconds to wait for response until executing `error_callback`.
+            * `timeout`: Milliseconds to wait for response until executing `errorCallback`.
                 - type: number
-                - default: `this.default_timeout`
-            * `error_callback`: Function to be executed on request errors.
+                - default: `this.defaultTimeout`
+            * `errorCallback`: Function to be executed on request errors.
                 - type: function
                 - default: Throws error
         
@@ -93,21 +93,21 @@ class compendium {
                 }
                 callback(data);
             })
-        }).on("error", error_callback)
+        }).on("error", errorCallback)
         req.setTimeout(timeout, req.destroy);
     }
 
-    get_all(callback: Function, timeout: number=this.default_timeout, error_callback: Function=function(err: any){throw err}){
+    getAll(callback: Function, timeout: number=this.defaultTimeout, errorCallback: Function=function(err: any){throw err}){
         /*
         Get all entries from the compendium.
 
         Parameters:
             * `callback`: Function to be executed with all items in the compendium with their metadata, nested in categories.
                 - type: function
-            * `timeout`: Milliseconds to wait for response until executing `error_callback`.
+            * `timeout`: Milliseconds to wait for response until executing `errorCallback`.
                 - type: number
-                - default: `this.default_timeout`
-            * `error_callback`: Function to be executed on request errors.
+                - default: `this.defaultTimeout`
+            * `errorCallback`: Function to be executed on request errors.
                 - type: function
                 - default: Throws error
         */
@@ -122,32 +122,32 @@ class compendium {
                 data=JSON.parse(data)["data"];
                 callback(data)
             })
-        }).on("error", error_callback)
+        }).on("error", errorCallback)
         req.setTimeout(timeout, req.destroy);
     }
 
-    download_entry_image(entry: string|number, output_file: string|null=null, callback=function(){}, timeout: number=this.default_timeout, error_callback: Function=function(err: any){throw err}){
+    downloadEntryImage(entry: string|number, outputFile: string|null=null, callback=function(){}, timeout: number=this.defaultTimeout, errorCallback: Function=function(err: any){throw err}){
         /*
         Download the image of a compendium entry.
 
         Parameters:
             * `entry`: The ID or name of the entry of the image to be downloaded.
                 - type: str, int
-            * `output_file`: The output file's path.
+            * `outputFile`: The output file's path.
                 - type: str
                 - default: entry's name with a ".png" extension with spaces replaced with underscores
             * `callback`: The function to executed with image binary
                 type: function
                 default: `function(){}` (empty function)
-            * `timeout`: Milliseconds to wait for server response until executing `error_callback`.
+            * `timeout`: Milliseconds to wait for server response until executing `errorCallback`.
                 - type: number
-                - default: `this.default_timeout`
-            * `error_callback`: Function to be executed on request errors.
+                - default: `this.defaultTimeout`
+            * `errorCallback`: Function to be executed on request errors.
                 - type: function
                 - default: Throws error
         */
         
-        this.get_entry(entry, function(data: any) {
+        this.getEntry(entry, function(data: any) {
             let req = https.get(data["image"], (resp: any) => {
                     let strm = new Stream();
                     resp.on("data", (chunk: string) => {
@@ -155,14 +155,12 @@ class compendium {
                     });
         
                     resp.on("end", () => {
-                        fs.writeFile(output_file ?? (data["name"]+".png").replace(" ", "_"), strm.read(), callback)
+                        fs.writeFile(outputFile ?? (data["name"]+".png").replace(" ", "_"), strm.read(), callback)
                     })
-               }).on("error", error_callback)
+               }).on("error", errorCallback)
             req.on("timeout", req.destroy)
-        }, timeout, error_callback)
+        }, timeout, errorCallback)
     }
 }
 
-exports.NoEntryError = NoEntryError
-exports.NoCategoryError = NoCategoryError
-exports.compendium = compendium
+export { NoCategoryError, NoEntryError }
