fce85eb07d6f6ae008ec334b22f48f3820a7256e
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-06-19 10:42:41 -0700

    Remove dummy files

--- src/ts.ts
+++
@@ -1,8 +0,0 @@
-import {compendium} from "./api"
-let comp = new compendium()
-
-describe("things", ()=>{
-    it("should yoit", (done) => {
-        comp.get_entry(1, ()=>{}, 100/100/100, ()=>{done()})
-    })
-})
